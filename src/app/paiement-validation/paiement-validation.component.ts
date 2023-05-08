12309e52ba4f7adefd1a568a43b58b8f49f03036
hamdanimed <hamdanimee@gmail.com>
2023-05-08 13:06:27 +0100

    minor changes

--- src/app/paiement-validation/paiement-validation.component.ts
+++ src/app/paiement-validation/paiement-validation.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class PaiementValidationComponent {
   impayeList:{name:string,value:number}[]=[
     {name:"Recharge Voix",value:100},
-    // {name:"Recharge Voix",value:100},
-    // {name:"Recharge Voix",value:100},
-    // {name:"Recharge Voix",value:100},
+    {name:"Recharge Voix",value:100},
+    {name:"Recharge Voix",value:100},
+    {name:"Recharge Voix",value:100},
   ];
 }
